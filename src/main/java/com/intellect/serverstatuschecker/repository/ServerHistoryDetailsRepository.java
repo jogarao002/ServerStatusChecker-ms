ba4fcccd8cbf7265a92d9b47232144452c4a8425
saikumar.kotturu <saikumar.kotturu@intellectinfo.in>
2024-12-24 17:08:37 +0530

    add historyDetails

---
+++ src/main/java/com/intellect/serverstatuschecker/repository/ServerHistoryDetailsRepository.java
@@ -0,0 +1,13 @@
+package com.intellect.serverstatuschecker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.intellect.serverstatuschecker.domain.MailDetails;
+import com.intellect.serverstatuschecker.domain.ServerHistoryDetails;
+
+@SuppressWarnings("unused")
+@Repository
+public interface ServerHistoryDetailsRepository extends JpaRepository<ServerHistoryDetails, Long> {
+
+}
